ee473718e8b85eed40b02c0e324ea63e555e3e68
Arihant416 <arihantjain416@gmail.com>
2020-04-02 00:30:24 +0530

    Handle errors gracefully

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,17 +5,30 @@ class App extends React.Component {
   //The very first function to be called is the constructor
   constructor(props) {
     super(props);
-
-    this.state = {lat: null};
+    //This is the only time we do direct assignment to this.state
+    this.state = {lat: null, errorMessage: ''};
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //We Called setState !!!to update the state
+        this.setState({lat: position.coords.latitude});
+        //We did not !!!
+        // this .state.lat=position.coords.latitude
+      },
+      err => {
+        this.setState({errorMessage: err.message});
+      }
+    );
   }
 
   //React says we have to define a render method
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error:{this.state.errorMessage}
+      </div>
     );
-    return <div>Latitude:</div>;
   }
 }
 
