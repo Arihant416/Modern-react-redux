51d84cbe08fd0ef559838efe64ad4e565ef999e9
Arihant416 <arihantjain416@gmail.com>
2020-04-02 00:01:22 +0530

    Initialized State through Constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  //The very first function to be called is the constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {lat: null};
+  }
+
+  //React says we have to define a render method
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
