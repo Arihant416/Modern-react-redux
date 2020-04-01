b1bdb91ef402ac59ecb5e635babe75d2fbc74da7
Arihant416 <arihantjain416@gmail.com>
2020-04-01 17:16:44 +0530

    Component Improvised

--- components/src/index.js
+++ components/src/index.js
@@ -2,35 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      {/*Components are an exception if we are to nest it within other components we have to tag it not add it in curly braces. */}
-      <CommentDetail
-        author='Sam'
-        momentofCreation='Today at 4:50PM'
-        comment='I am Sam'
-        faker={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Alex'
-        momentofCreation='Today at 2:30PM'
-        comment='I am Alex'
-        faker={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Bootstrap'
-        momentofCreation='Today at 1:20PM'
-        comment='I am Bootstrap'
-        faker={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Jane'
-        momentofCreation='Today at 1:50AM'
-        comment='I am Jane'
-        faker={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        {/*Components are an exception if we are to nest it within other components we have to tag it not add it in curly braces. */}
+        <CommentDetail
+          author='Sam'
+          momentofCreation='Today at 4:50PM'
+          comment='I am Sam'
+          faker={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          momentofCreation='Today at 2:30PM'
+          comment='I am Alex'
+          faker={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Bootstrap'
+          momentofCreation='Today at 1:20PM'
+          comment='I am Bootstrap'
+          faker={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          momentofCreation='Today at 1:50AM'
+          comment='I am Jane'
+          faker={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
