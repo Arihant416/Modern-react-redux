b5e5dcbdeffeeb25bc04124bf41410475a7ab9fa
Arihant416 <arihantjain416@gmail.com>
2020-04-02 12:56:59 +0530

    Add some styling and configuration

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,35 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
   //The very first function to be called is the constructor
-  constructor(props) {
-    super(props);
-    //This is the only time we do direct assignment to this.state
-    this.state = {lat: null, errorMessage: ''};
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        //We Called setState !!!to update the state
-        this.setState({lat: position.coords.latitude});
-        //We did not !!!
-        // this .state.lat=position.coords.latitude
-      },
-      err => {
-        this.setState({errorMessage: err.message});
-      }
+
+  state = {lat: null, errorMessage: ''};
+
+  componentDidMount () {
+    window.navigator.geolocation.getCurrentPosition (
+      position => this.setState ({lat: position.coords.latitude}),
+      err => this.setState ({errorMessage: err.message})
     );
   }
 
   //React says we have to define a render method
-  render() {
+  render () {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading ...</div>;
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render (<App />, document.querySelector ('#root'));
