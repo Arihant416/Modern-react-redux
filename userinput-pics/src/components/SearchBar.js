fc5ae88f4e492354a2c1b72a8db5f83f3282ca05
Arihant416 <arihantjain416@gmail.com>
2020-04-03 10:42:28 +0530

    Before Handling form submittal

--- userinput-pics/src/components/SearchBar.js
+++ userinput-pics/src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+  state = {term: 'Well,Hello there!'};
+
   render() {
     return (
       <div className='ui segment'>
         <form className='ui form'>
           <div className='field'>
             <label>Image Search</label>
-            <input type='text' onChange={e => console.log(e.target.value)} />
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={e => this.setState({term: e.target.value})}
+            />
           </div>
         </form>
       </div>
