9a3ab059d287a21e988d259afc1d19d06c70f350
Arihant416 <arihantjain416@gmail.com>
2020-04-06 13:34:13 +0530

    Youtube API

---
+++ YoutubeAPI/src/APIs/youtube.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// const KEY = 'AIzaSyAj-EgqjfMEJVh5epBOaucCz5wwUk8e6_c';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+
+  //
+});
